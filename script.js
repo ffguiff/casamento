74bc823e6fd82ffb8a28b008141b7a4ec84619ca
Guilherme Ferreira <90099945+ffguiff@users.noreply.github.com>
2025-03-14 22:29:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function cerimonia(){
     window.open("https://www.google.com/maps/place/16%C2%B009'31.1%22S+48%C2%B054'31.0%22W/@-16.158638,-48.908615,17z/data=!3m1!4b1!4m4!3m3!8m2!3d-16.158638!4d-48.908615?entry=ttu&g_ep=EgoyMDI1MDMxMi4wIKXMDSoASAFQAw%3D%3D", '_blank')
 }
 function pix(){
-    window.open("")
+    window.open("https://nubank.com.br/cobrar/oqgdx/67d4d527-4901-47d5-8c97-57244533e701")
 }
 function presente(){
     window.open("")
