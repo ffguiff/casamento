802be5cc659aedee85441f9f1d73535cf5d22ec9
Guilherme Ferreira <90099945+ffguiff@users.noreply.github.com>
2025-03-14 22:02:28 -0300

    convite de casamento

---
+++ script.js
@@ -0,0 +1,12 @@
+function cerimonia(){
+    window.open("https://www.google.com/maps/place/16%C2%B009'31.1%22S+48%C2%B054'31.0%22W/@-16.158638,-48.908615,17z/data=!3m1!4b1!4m4!3m3!8m2!3d-16.158638!4d-48.908615?entry=ttu&g_ep=EgoyMDI1MDMxMi4wIKXMDSoASAFQAw%3D%3D", '_blank')
+}
+function pix(){
+    window.open("")
+}
+function presente(){
+    window.open("")
+}    
+function whatsapp(){
+    window.open("https://api.whatsapp.com/send?phone=%205562994513329&text=%2AConfirmo+minha+presen%C3%A7a%21%F0%9F%A5%B3%2A")
+}
